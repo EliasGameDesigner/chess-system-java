b549896deae0d4f5d9422c5bec5c970f536eef29
Elias <elias.souza@cingo.com>
2025-07-02 17:57:14 -0300

    Switching player each turn

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,8 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		while(true) {
 			try {
-				UI.printBoard(chessMatch.getPieces());
+				UI.clearScreen();
+				UI.printMatch(chessMatch);
 				System.out.println();
 				System.out.println("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,12 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch {
 
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
-
+	
+	public  int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
+	}
+	
 	public ChessMatch() {
-
 		board = new Board(8, 8);
 		initialSetup();
+		turn = 1;
+		currentPlayer = Color.WHITE;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -33,6 +44,7 @@ public class ChessMatch {
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -47,6 +59,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if(currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosemn piece is not yours");
+		}
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chose piece");
 		}
@@ -56,6 +71,11 @@ public class ChessMatch {
 		if(!board.piece(source).possibleMove(target)) {
 			throw new ChessException("The Chosen piece can't move to target position");
 		}
+	}
+	
+	public void nextTurn() {
+		turn++;
+		currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE;
 	}
 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
