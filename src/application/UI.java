8c94c28d35af72a9f1edf46f310627a9e6cae600
Elias <elias.souza@cingo.com>
2025-07-02 10:57:15 -0300

    Moving Pieces and Some Exception

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.Color;
 
 public class UI {
@@ -36,6 +40,8 @@ public class UI {
 		}
 		System.out.println("  a b c d e f g h");
 	}
+	
+	
 
 	private static void printPiece(ChessPiece piece) {
 		if (piece == null) {
@@ -48,5 +54,18 @@ public class UI {
 			}
 		}
 		System.out.print(" ");
+	}
+	
+	public static ChessPosition readChessPosition(Scanner sc) {
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column,row);
+			
+		}catch(RuntimeException e){
+			throw new InputMismatchException("Error reading ChessPosition, valid values are frin a1 ti h8 ");
+		}
+		
 	}
 }
